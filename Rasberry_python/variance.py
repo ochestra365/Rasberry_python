0ff229eea0ca90878e589fff79341af4c9eb8ac1
ochestra365 <ochestra365@naver.com>
2021-05-27 13:32:47 +0900

    commit

---
+++ Rasberry_python/variance.py
@@ -0,0 +1,23 @@
+# Variance
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+LED=22
+BUTTON=4
+BUTTON_PRESSED=0
+ON=1
+OFF=0
+
+GPIO.setup(LED,GPIO.OUT)
+GPIO.setup(BUTTON,GPIO.IN)
+while 1:
+    if(GPIO.input(BUTTON)==BUTTON_PRESSED):
+        GPIO.ouput(LED,ON)
+    else:
+        GPIO.output(LED,OFF)
+    button_value=GPIO.input(BUTTON)
+
+    if(button_value==BUTTON_PRESSED) :
+        GPIO.output(LED,ON)
+    else:
+        GPIO.output(LED,OFF)
